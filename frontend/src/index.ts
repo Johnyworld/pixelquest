0e7cca64c1851bd3efb45ac82163018d36e3643b
johnyworld <johnyworld@naver.com>
2020-09-21 22:02:56 +0900

    Add keyboard events for test

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -3,6 +3,9 @@ const socket = io('http://localhost:7000');
 
 socket.on('connect', () => console.log('connexted!!!'));
 
+const SCREEN_WIDTH = 640;
+const SCREEN_HEIGHT = 480;
+
 interface Vec2Interface {
   x: number;
   y: number;
@@ -50,19 +53,32 @@ class Entity {
 }
 
 const canvas = <HTMLCanvasElement> document.getElementById('canvas');
-canvas.width = 640;
-canvas.height = 640;
+canvas.width = SCREEN_WIDTH;
+canvas.height = SCREEN_HEIGHT;
 const ctx = canvas.getContext('2d')!;
-
-// ctx?.fillRect(0, 0, 100, 100);
 
 const player = new Entity(ctx, 50, 50);
 
-ctx.fillStyle = '#e5e5e5';
-ctx.fillRect(0, 0, 640, 640);
+window.addEventListener('keydown', (e:any) => {
+  e.preventDefault();
+  console.log(e.code);
+  if ( e.code === 'KeyA' ) {
+    player.vel = { x: -1, y: 0 };
+  } else if ( e.code === 'KeyS' ) {
+    player.vel = { x: 0, y: 1 };
+  } else if ( e.code === 'KeyD' ) {
+    player.vel = { x: 1, y: 0 };
+  } else if ( e.code === 'KeyW' ) {
+    player.vel = { x: 0, y: -1 };
+  } else if ( e.code === 'Space' ) {
+    player.vel = { x: 0, y: 0 };
+  }
+})
 
 const update = () => {
-  ctx.clearRect(0, 0, 640, 640);
+  ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+  ctx.fillStyle = '#e5e5e5';
+  ctx.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
   player.update(ctx);
   requestAnimationFrame(update);
 }
