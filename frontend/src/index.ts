059193984edf90114e7b538b619287ec07c091b3
johnyworld <johnyworld@naver.com>
2020-09-22 00:54:08 +0900

    Testing socket server with data networking

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,8 +1,6 @@
 declare const io: typeof import('socket.io');
 const socket = io('http://localhost:7000');
 
-socket.on('connect', () => console.log('connexted!!!'));
-
 const SCREEN_WIDTH = 640;
 const SCREEN_HEIGHT = 480;
 
@@ -57,31 +55,42 @@ canvas.width = SCREEN_WIDTH;
 canvas.height = SCREEN_HEIGHT;
 const ctx = canvas.getContext('2d')!;
 
-const player = new Entity(ctx, 50, 50);
+// const player = new Entity(ctx, 50, 50);
 
-window.addEventListener('keydown', (e:any) => {
-  e.preventDefault();
-  console.log(e.code);
-  if ( e.code === 'KeyA' ) {
-    player.vel = { x: -1, y: 0 };
-  } else if ( e.code === 'KeyS' ) {
-    player.vel = { x: 0, y: 1 };
-  } else if ( e.code === 'KeyD' ) {
-    player.vel = { x: 1, y: 0 };
-  } else if ( e.code === 'KeyW' ) {
-    player.vel = { x: 0, y: -1 };
-  } else if ( e.code === 'Space' ) {
-    player.vel = { x: 0, y: 0 };
-  }
-})
+// window.addEventListener('keydown', (e:any) => {
+//   e.preventDefault();
+//   console.log(e.code);
+//   if ( e.code === 'KeyA' ) {
+//     player.vel = { x: -1, y: 0 };
+//   } else if ( e.code === 'KeyS' ) {
+//     player.vel = { x: 0, y: 1 };
+//   } else if ( e.code === 'KeyD' ) {
+//     player.vel = { x: 1, y: 0 };
+//   } else if ( e.code === 'KeyW' ) {
+//     player.vel = { x: 0, y: -1 };
+//   } else if ( e.code === 'Space' ) {
+//     player.vel = { x: 0, y: 0 };
+//   }
+// })
 
-const update = () => {
-  ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-  ctx.fillStyle = '#e5e5e5';
-  ctx.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-  player.update(ctx);
-  requestAnimationFrame(update);
+// const update = () => {
+//   ctx.fillStyle = '#e5e5e5';
+//   ctx.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+//   player.update(ctx);
+// }
+
+const newConnect = () => {
+  socket.emit('newConnect');
 }
 
-update();
+const handleGameState = (data:any) => {
+  ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+  for ( const player of data.players ) {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(player.pos.x, player.pos.y, player.size.width, player.size.height);
+  }
+}
+
+socket.on('gameState', handleGameState);
+newConnect();
 
--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,69 @@
+import { start } from "repl";
 import * as socketio from "socket.io";
 
 let io = socketio(7000);
 
-io.on("connection", function(socket: any) {
-  console.log("a user connected");
+const FRAME_RATE = 120;
+
+interface Vec2Interface {
+  x: number;
+  y: number;
+}
+
+interface SizeInterface {
+  width: number;
+  height: number;
+}
+
+class Vec2 implements Vec2Interface {
+  x: number;
+  y: number;
+  constructor(x:number, y:number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Entity {
+  ctx: CanvasRenderingContext2D;
+  pos: Vec2Interface;
+  vel: Vec2Interface;
+  size: SizeInterface;
+  constructor(x:number, y:number) {
+    this.pos = new Vec2(x, y);
+    this.vel = new Vec2(1, 0);
+    this.size = {
+      width: 32,
+      height: 32,
+    }
+  }
+  update() {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+  }
+}
+
+const state = {
+  players: []
+}
+
+
+const startGameInterval = () => {
+  const gameInterval = setInterval(() => {
+    for ( const player of state.players ) {
+      player.update();
+    }
+    io.emit('gameState', state);
+  }, 1000 / FRAME_RATE)
+}
+
+io.on("connection", client => {
+  
+  const handleNewConnect = () => {
+    state.players.push( new Entity(30, 30) );
+    console.log('newConnect!');
+    startGameInterval();
+  }
+
+  client.on('newConnect', handleNewConnect);
 });
