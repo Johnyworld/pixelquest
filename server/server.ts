580c5e280ac500bf29165b9e73c10bdedec13902
johnyworld <johnyworld@naver.com>
2020-09-21 21:39:32 +0900

    Remove testing code

--- server/server.ts
+++ server/server.ts
@@ -3,10 +3,5 @@ import * as socketio from "socket.io";
 let io = socketio(7000);
 
 io.on("connection", function(socket: any) {
-  socket.on('hello', () => {
-    console.log('hello come')
-    socket.emit('connect');
-  })
-
   console.log("a user connected");
 });
